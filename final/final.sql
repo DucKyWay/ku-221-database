49a9e4f2391a52bf760ec00db18678080a6cbb19
DucKyWay <niji2549@gmail.com>
2026-03-02 14:14:36 +0700

    finish data for exam

--- final/final.sql
+++ final/final.sql
@@ -1,11 +1,32 @@
-CREATE TABLE `supplier` (
-    `S_no` char(2) NOT NULL,
+/*
+1. Which supplier lives in Sumrong and supplies red color part? List Sname and Pname
+2. Which part is supplied by all suppliers? List Pname
+3. Which supplier has the second maximum status? List Sname and status
+4. List Sname for the supplier who supplies the third maximum quantities
+*/
+
+/*
+SELECT SNAME, CITY 
+FROM SUPPLIER 
+WHERE S_NO IN (SELECT S_NO 
+               FROM SUPPLY
+               GROUP BY S_NO
+               HAVING SUM(QTY) = (SELECT MIN(A) FROM (SELECT SUM(QTY) A FROM SUPPLY GROUP BY S_NO) A));
+               */
+
+-- SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+-- SET AUTOCOMMIT = 0;
+-- START TRANSACTION;
+-- SET time_zone = "+00:00";
+
+CREATE TABLE `SUPPLIER` (
+    `S_NO` varchar(40) NOT NULL,
     `SNAME` varchar(40),
     `CITY` varchar(40),
-    `STATUS` INT(11),
+    `STATUS` INT(11)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `supplier` (`S_no`, `SNAME`, `CITY`, `STATUS`) VALUES 
+INSERT INTO `SUPPLIER` (`S_NO`, `SNAME`, `CITY`, `STATUS`) VALUES 
 ('S1', 'David H', 'London', 100),
 ('S2', 'Wimut W', 'Rayong', 200),
 ('S3', 'Somchai S', 'Sumrong', 200),
@@ -15,13 +36,13 @@ INSERT INTO `supplier` (`S_no`, `SNAME`, `CITY`, `STATUS`) VALUES
 ('S7', 'Chavalit Y', 'Nakornpanom', 10),
 ('S8', 'Suchinda K', 'Helicity', 1);
 
-CREATE TABLE `part` (
-    `P_no` CHAR(2) NOT NULL,
+CREATE TABLE `PART` (
+    `P_NO` varchar(40) NOT NULL,
     `PNAME` VARCHAR(40),
-    `COLOR` VARCHAR(40),
+    `COLOR` VARCHAR(40)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `part` (`P_no`, `PNAME`, `COLOR`) VALUES
+INSERT INTO `PART` (`P_NO`, `PNAME`, `COLOR`) VALUES
 ('P1', 'NUT', 'RED'),
 ('P2', 'CASE', 'AMBER'),
 ('P3', 'SCREW', 'YELLOW'),
@@ -34,24 +55,24 @@ INSERT INTO `part` (`P_no`, `PNAME`, `COLOR`) VALUES
 ('P10', 'Outlet', 'Cream'),
 ('P11', 'Harddisk', 'Black');
 
-CREATE TABLE `supply` (
-    `S_no` CHAR(2) NOT NULL,
-    `P_no` CHAR(2) NOT NULL,
+CREATE TABLE `SUPPLY` (
+    `S_NO` varchar(40) NOT NULL,
+    `P_NO` varchar(40) NOT NULL,
     `QTY` INT(11)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `supply` (`S_no`, `P_no`, `QTY`) VALUES
-('S1', 'P3', '100'),
-('S1', 'P1', '900'),
-('S2', 'P7', '100'),
-('S2', 'P9', '200'),
-('S2', 'P1', '300'),
-('S3', 'P1', '300'),
-('S3', 'P4', '400'),
-('S3', 'P2', '500'),
-('S3', 'P10', '300'),
-('S4', 'P6', '800'),
-('S5', 'P6', '900'),
-('S6', 'P8', '400'),
-('S7', 'P3', '800'),
-('S8', 'P5', '100');
+INSERT INTO `SUPPLY` (`S_NO`, `P_NO`, `QTY`) VALUES
+('S1', 'P3', 100),
+('S1', 'P1', 900),
+('S2', 'P7', 100),
+('S2', 'P9', 200),
+('S2', 'P1', 300),
+('S3', 'P1', 300),
+('S3', 'P4', 400),
+('S3', 'P2', 500),
+('S3', 'P10', 300),
+('S4', 'P6', 800),
+('S5', 'P6', 900),
+('S6', 'P8', 400),
+('S7', 'P3', 800),
+('S8', 'P5', 100);
