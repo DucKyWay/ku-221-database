fb25d8b36431a219e5a02ee93cdfd3d0f6ea101c
DucKyWay <niji2549@gmail.com>
2026-03-02 13:40:56 +0700

    add final data

---
+++ final/final.sql
@@ -0,0 +1,57 @@
+CREATE TABLE `supplier` (
+    `S_no` char(2) NOT NULL,
+    `SNAME` varchar(40),
+    `CITY` varchar(40),
+    `STATUS` INT(11),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `supplier` (`S_no`, `SNAME`, `CITY`, `STATUS`) VALUES 
+('S1', 'David H', 'London', 100),
+('S2', 'Wimut W', 'Rayong', 200),
+('S3', 'Somchai S', 'Sumrong', 200),
+('S4', 'Clinton B', 'Newyork', 100),
+('S5', 'Chuan L', 'Bangkok', 10),
+('S6', 'Chumlong S', 'Bangkok', 10),
+('S7', 'Chavalit Y', 'Nakornpanom', 10),
+('S8', 'Suchinda K', 'Helicity', 1);
+
+CREATE TABLE `part` (
+    `P_no` CHAR(2) NOT NULL,
+    `PNAME` VARCHAR(40),
+    `COLOR` VARCHAR(40),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `part` (`P_no`, `PNAME`, `COLOR`) VALUES
+('P1', 'NUT', 'RED'),
+('P2', 'CASE', 'AMBER'),
+('P3', 'SCREW', 'YELLOW'),
+('P4', 'SPEEKER', 'BLACK'),
+('P5', 'Gun', 'Gray'),
+('P6', 'Meetball', 'Gray'),
+('P7', 'Book', 'Blue'),
+('P8', 'Deli', 'Buffy'),
+('P9', 'Rubber', 'White'),
+('P10', 'Outlet', 'Cream'),
+('P11', 'Harddisk', 'Black');
+
+CREATE TABLE `supply` (
+    `S_no` CHAR(2) NOT NULL,
+    `P_no` CHAR(2) NOT NULL,
+    `QTY` INT(11)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `supply` (`S_no`, `P_no`, `QTY`) VALUES
+('S1', 'P3', '100'),
+('S1', 'P1', '900'),
+('S2', 'P7', '100'),
+('S2', 'P9', '200'),
+('S2', 'P1', '300'),
+('S3', 'P1', '300'),
+('S3', 'P4', '400'),
+('S3', 'P2', '500'),
+('S3', 'P10', '300'),
+('S4', 'P6', '800'),
+('S5', 'P6', '900'),
+('S6', 'P8', '400'),
+('S7', 'P3', '800'),
+('S8', 'P5', '100');
